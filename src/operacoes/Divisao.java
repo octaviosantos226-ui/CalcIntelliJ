179353a2a1a14e24c13786a876e92aee25bf0d21
Octavio Henrique <octaviosantos225@gmail.com>
2026-05-27 14:29:44 -0300

    feat: implementa funcao divisao

---
+++ src/operacoes/Divisao.java
@@ -0,0 +1,14 @@
+package operacoes;
+
+import java.util.Scanner;
+
+public class Divisao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("digite o primeiro valor");
+        int x = scanner.nextInt();
+        System.out.println("digite o segundo valor");
+        int y = scanner.nextInt();
+        System.out.println("o resultado é:" + (x * y));
+    }
+}
