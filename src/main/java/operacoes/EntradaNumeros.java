89719d2dc737a8c473935f178e287cc7a9e257d5
Octavio Henrique <octaviosantos225@gmail.com>
2026-06-10 13:24:23 -0300

    feat: adiciona funcao entrada de numeros

---
+++ src/main/java/operacoes/EntradaNumeros.java
@@ -0,0 +1,21 @@
+
+        package operacoes;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author info1
+ */
+public class EntradaNumeros {
+    int x;
+    int y;
+
+    public void pedir(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("digite o primeiro valor");
+        x = scanner.nextInt();
+        System.out.println("digite o segundo valor");
+        y = scanner.nextInt();
+    }
+}
